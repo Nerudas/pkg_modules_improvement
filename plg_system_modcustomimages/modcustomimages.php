afc8bb6bf3699a7526391cce56852b909596d990
Septdir <septdir@gmail.com>
2018-09-18 12:42:53 +0300

    Change images field

--- plg_system_modcustomimages/modcustomimages.php
+++ plg_system_modcustomimages/modcustomimages.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    System - Custom Module Images Plugin
- * @version    1.0.1
+ * @version    1.1.0
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
@@ -13,10 +13,9 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
-use Joomla\CMS\Uri\Uri;
 use Joomla\Registry\Registry;
 
-jimport('joomla.filesystem.folder');
+JLoader::register('FieldTypesFilesHelper', JPATH_PLUGINS . '/fieldtypes/files/helper.php');
 
 class plgSystemModCustomImages extends CMSPlugin
 {
@@ -29,31 +28,13 @@ class plgSystemModCustomImages extends CMSPlugin
 	protected $autoloadLanguage = true;
 
 	/**
-	 * Imagefolder helper helper
+	 * Images root path
 	 *
-	 * @var    new imageFolderHelper
+	 * @var    string
 	 *
-	 * @since 1.0.0
+	 * @since  1.2.0
 	 */
-	protected $imageFolderHelper = null;
-
-	/**
-	 * Constructor
-	 *
-	 * @param   object &$subject   The object to observe
-	 * @param   array  $config     An optional associative array of configuration settings.
-	 *                             Recognized key values include 'name', 'group', 'params', 'language'
-	 *                             (this list is not meant to be comprehensive).
-	 *
-	 * @since 1.0.0
-	 */
-	public function __construct($subject, array $config = array())
-	{
-		JLoader::register('imageFolderHelper', JPATH_PLUGINS . '/fieldtypes/ajaximage/helpers/imagefolder.php');
-		$this->imageFolderHelper = new imageFolderHelper('images/modules');
-
-		parent::__construct($subject, $config);
-	}
+	protected $images_root = 'images/modules';
 
 	/**
 	 * Add Advanced Assignment fields
@@ -70,7 +51,6 @@ class plgSystemModCustomImages extends CMSPlugin
 		$app = Factory::getApplication();
 
 		$moduleData = new Registry($data);
-		$moduleId   = $moduleData->get('id', 0);
 
 		if ($app->isAdmin() && $app->input->get('option') == 'com_modules' &&
 			$form->getName() == 'com_modules.module' && $moduleData->get('module') == 'mod_custom')
@@ -78,21 +58,8 @@ class plgSystemModCustomImages extends CMSPlugin
 			Form::addFormPath(__DIR__);
 			$form->loadFile('form', false);
 
-			$saveurl = Uri::base(true) . '/index.php?option=com_ajax&plugin=modcustomimages&group=system&format=json&id='
-				. $moduleId . '&field=images';
-			$form->setFieldAttribute('images', 'saveurl', $saveurl . 'images');
-
-			if (!empty($moduleId))
-			{
-				$db    = Factory::getDbo();
-				$query = $db->getQuery(true)
-					->select('images')
-					->from('#__modcustomimages')
-					->where('id = ' . $moduleId);
-				$db->setQuery($query);
-
-				$form->setValue('images', '', $db->loadResult());
-			}
+			// Set images folder root
+			$form->setFieldAttribute('images_folder', 'root', $this->images_root);
 		}
 
 		return true;
@@ -102,35 +69,25 @@ class plgSystemModCustomImages extends CMSPlugin
 	 * Method to save images
 	 *
 	 * @param string  $context
-	 * @param object  $item
+	 * @param object  $extension
 	 * @param boolean $isNew
 	 *
 	 * @return boolean
 	 *
 	 * @since 1.0.0
 	 */
-	function onExtensionAfterSave($context, $item, $isNew)
+	function onExtensionAfterSave($context, $extension, $isNew)
 	{
-		if ($context == 'com_modules.module' && $item->module == 'mod_custom')
+		if ($context == 'com_modules.module' && $extension->module == 'mod_custom')
 		{
 			$data = Factory::getApplication()->input->post->get('jform', array(), 'array');
 
-			$id = $item->id;
-
 			// Save images
-			$data['imagefolder'] = (!empty($data['imagefolder'])) ? $data['imagefolder'] :
-				$this->imageFolderHelper->getItemImageFolder($id);
-
-			if ($isNew)
+			if ($isNew && !empty($data['images_folder']))
 			{
-				$data['images'] = (isset($data['images'])) ? $data['images'] : array();
+				$filesHelper = new FieldTypesFilesHelper();
+				$filesHelper->moveTemporaryFolder($data['images_folder'], $extension->id, $this->images_root);
 			}
-
-			if (isset($data['images']))
-			{
-				$this->imageFolderHelper->saveItemImages($id, $data['imagefolder'], '#__modcustomimages', 'images', $data['images']);
-			}
-
 		}
 
 		return true;
@@ -140,92 +97,18 @@ class plgSystemModCustomImages extends CMSPlugin
 	 * Method for delete imageFolder
 	 *
 	 * @param string $context
-	 * @param object $item
+	 * @param object $extension
 	 *
 	 * @return void
 	 *
 	 * @since 1.0.0
 	 */
-	function onExtensionAfterDelete($context, $item)
+	function onExtensionAfterDelete($context, $extension)
 	{
-		if ($context == 'com_modules.module' && $item->module == 'mod_custom')
+		if ($context == 'com_modules.module' && $extension->module == 'mod_custom')
 		{
-			$this->imageFolderHelper->deleteItemImageFolder($item->id);
+			$filesHelper = new FieldTypesFilesHelper();
+			$filesHelper->deleteItemFolder($extension->id, $this->images_root);
 		}
-	}
-
-	/**
-	 * Ajax Save images
-	 *
-	 * @return void
-	 *
-	 * @since 1.0.0
-	 */
-	public function onAjaxModCustomImages()
-	{
-		$app   = Factory::getApplication();
-		$id    = $app->input->get('id', 0, 'int');
-		$value = $app->input->get('value', '', 'raw');
-		if (!empty($id))
-		{
-			$this->imageFolderHelper->saveImagesValue($id, '#__modcustomimages', 'images', $value);
-		}
-	}
-
-
-	/**
-	 *  Advanced Assignment check in modules array
-	 *
-	 * @param   $modules  The module object.
-	 *
-	 * @return  void
-	 *
-	 * @since 1.0.0
-	 */
-	function onAfterModuleList(&$modules)
-	{
-		foreach ($modules as $key => &$module)
-		{
-			$module = $this->replaceImageFolder($module);
-		}
-	}
-
-	/**
-	 * Advanced Assignment check on render module
-	 *
-	 * @param  object $module  The module object.
-	 * @param  array  $attribs The render attributes
-	 *
-	 * @return  void
-	 *
-	 * @since 1.0.0
-	 */
-	function onRenderModule(&$module, &$attribs)
-	{
-		$module = $this->replaceImageFolder($module);
-
-		return;
-	}
-
-	/**
-	 * Method to replace {imageFolder}
-	 *
-	 * @param  object $module The module object.
-	 *
-	 * @return object
-	 *
-	 * @since 1.0.0
-	 */
-	protected function replaceImageFolder($module)
-	{
-		if ($module->module == 'mod_custom')
-		{
-			$module->imageFolder = (!empty($module->imageFolder)) ? $module->imageFolder :
-				$this->imageFolderHelper->getItemImageFolder($module->id);
-
-			$module->content = str_replace('{imageFolder}', $module->imageFolder, $module->content);
-		}
-
-		return $module;
 	}
 }
--- plg_system_modcustomimages/sql/install.mysql.utf8.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__modcustomimages` (
-  `id`           INT(11)          NOT NULL AUTO_INCREMENT,
-  `images`       LONGTEXT         NOT NULL DEFAULT '',
-  UNIQUE KEY `id` (`id`)
-)
-  ENGINE = MyISAM
-  DEFAULT CHARSET = utf8
-AUTO_INCREMENT = 0;
