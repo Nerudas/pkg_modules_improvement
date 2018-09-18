7f2f67a3b90b51860d88c8bd11921346cacb9460
Septdir <septdir@gmail.com>
2018-09-18 13:00:15 +0300

    Remove update script

--- plg_system_modcustomimages/script.php
+++ plg_system_modcustomimages/script.php
@@ -10,8 +10,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-
 class PlgSystemModCustomImagesInstallerScript
 {
 
@@ -32,62 +30,5 @@ class PlgSystemModCustomImagesInstallerScript
 		}
 
 		return true;
-	}
-
-	/**
-	 * update
-	 *
-	 * @param  \stdClass $parent - Parent object calling object.
-	 *
-	 * @return void
-	 *
-	 * @since  1.1.0
-	 */
-	public function update($parent)
-	{
-		$db = Factory::getDbo();
-
-		// Remove table
-		$db->setQuery($db->convertUtf8mb4QueryToUtf8('DROP TABLE IF EXISTS  `#__modcustomimages`'));
-		$db->execute();
-
-		// Replace shortcodes
-		$query = $db->getQuery(true)
-			->select(array('id', 'content'))
-			->from('#__modules')
-			->where($db->quoteName('module') . ' = ' . $db->quote('mod_custom'));
-		$db->setQuery($query);
-		$modules = $db->loadObjectList();
-
-
-		$images_root = 'images/modules';
-
-		foreach ($modules as $module)
-		{
-			$imagefolder     = $images_root . '/' . $module->id . '/content';
-			$module->content = str_replace('{imageFolder}', $imagefolder, $module->content);
-
-			$db->updateObject('#__modules', $module, array('id'));
-		}
-
-		$folders = JFolder::folders(JPATH_ROOT . '/' . $images_root);
-		foreach ($folders as $moduleID)
-		{
-			$moduleFolder  = JPATH_ROOT . '/' . $images_root . '/' . $moduleID;
-			$contentFolder = $moduleFolder . '/content';
-			if (!JFolder::exists($contentFolder))
-			{
-				JFolder::create($contentFolder);
-				JFile::write($contentFolder . '/index.html', '<!DOCTYPE html><title></title>');
-			}
-			$files = JFolder::files($moduleFolder, '', false);
-			foreach ($files as $file)
-			{
-				if ($file != 'index.html')
-				{
-					JFile::move($moduleFolder . '/' . $file, $contentFolder . '/' . $file);
-				}
-			}
-		}
 	}
 }
